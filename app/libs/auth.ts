90c10f3342021e95cad9bfb5be74848a33c81728
AALOK PATEL <aalok.patel@primedemo.in>
2024-10-21 16:37:43 +0530

    changed auth.ts

--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -1,3 +1,4 @@
+// libs/auth.ts
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
@@ -7,70 +8,57 @@ import bcrypt from "bcrypt";
 import db from "@/app/libs/prismadb";
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(db),
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID || "",
-            clientSecret: process.env.GITHUB_SECRET || "",
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-        }),
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                email: { label: "email", type: "text" },
-                password: { label: "password", type: "password" },
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error("Invalid credentials");
-                }
+  adapter: PrismaAdapter(db),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Invalid credentials");
+        }
 
-                const user = await db.user.findUnique({
-                    where: {
-                        email: credentials.email,
-                    },
-                });
+        const user = await db.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-                if (!user || !user.hashedPassword) {
-                    throw new Error("Invalid credentials");
-                }
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
 
-                const isCorrectPassword = await bcrypt.compare(
-                    credentials.password,
-                    user.hashedPassword
-                );
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
 
-                if (!isCorrectPassword) {
-                    throw new Error("Invalid credentials");
-                }
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
 
-                return user;
-            },
-        }),
-    ],
-    debug: process.env.NODE_ENV === "production",
-    session: {
-        strategy: "jwt",
-        maxAge: 30 * 24 * 60 * 60, // Example: 30 days
-        updateAge: 24 * 60 * 60, // Example: 24 hours
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: "/",
-        error: "/",
-    },
-    cookies: {
-        sessionToken: {
-            name: `next-auth.session-token`,
-            options: {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-                sameSite: 'None', // Allow cross-site cookie sending
-                path: '/', // Define the cookie path
-            },
-        },
-    },
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/",
+    error: "/",
+  },
 };
